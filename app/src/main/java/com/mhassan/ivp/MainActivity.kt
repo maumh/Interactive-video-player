4dd51f33bf7fb6c73566a3d990275e2af112ddfa
Mostafa Hassan <mostafa.hassan@maugames.com>
2021-09-25 16:56:30 +0200

    add exoplayer to the project
    1- added exoplayer to the project
    2-set the player to play the video in the task description
    3- set the player to start playing after 5 sec delay

--- app/src/main/java/com/mhassan/ivp/MainActivity.kt
+++ app/src/main/java/com/mhassan/ivp/MainActivity.kt
@@ -1,18 +1,45 @@
 package com.mhassan.ivp
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ui.PlayerView
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
+
+    // delay before video starts playing
+    val delayBeforeStart = 4000L;
+
+    // video url
+    val videoUri : String = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        val playerView : PlayerView = findViewById<PlayerView>(R.id.player_view);
+
+        val player = SimpleExoPlayer.Builder(this).build()
+        playerView.player = player;
+
+        val mediaItem: MediaItem = MediaItem.fromUri(videoUri)
+        player.setMediaItem(mediaItem);
+        player.prepare();
+
+        // start video play after delay.
+        runBlocking {
+            launch { // launch a new coroutine
+                delay(delayBeforeStart) // non-blocking delay
+                player.play() // start the video play
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
